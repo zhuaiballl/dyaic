fbb8672d3327905adb98c0ab3b405066e329514f
zhuaiballl <ghc@mail.bnu.edu.cn>
2021-12-22 20:37:10 +0800

    diff.go: output some log and time duration

--- diff/diff.go
+++ diff/diff.go
@@ -5,9 +5,12 @@ import (
 	"io/ioutil"
 	"log"
 	"os/exec"
+	"time"
 )
 
 func GetPatch(old, new, patchName string) {
+	fmt.Println(patchName, ": diff begin")
+	beginTime := time.Now()
 	patch, err := exec.Command("diff", old, new).Output()
 	fmt.Println(string(patch))
 	// diff returns exit code 1 if diff is found, should not panic this "error"
@@ -18,13 +21,18 @@ func GetPatch(old, new, patchName string) {
 	if err != nil {
 		log.Panic(err)
 	}
+	endTime := time.Now()
+	fmt.Println(patchName, ": diff finished in", endTime.Sub(beginTime))
 }
 
 func GetBSPatch(old, new, patchName string) {
 	cmd := exec.Command("bsdiff", old, new, patchName)
+	beginTime := time.Now()
+	fmt.Println(patchName, ": bsdiff begin")
 	err := cmd.Run()
-
 	if err != nil { // bsdiff returns 0 on success and -1 on failure
 		log.Panic(err)
 	}
+	endTime := time.Now()
+	fmt.Println(patchName, ": bsdiff finished in", endTime.Sub(beginTime))
 }
